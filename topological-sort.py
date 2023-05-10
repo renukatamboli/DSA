ba9f1c7f256156a988aee39aacf9b89c908467ae
renukatamboli <tambolirenuka2@gmail.com>
2023-05-10 08:50:25 +0530

    Create topological-sort.py

---
+++ topological-sort.py
@@ -0,0 +1,60 @@
+class Solution:
+    def dfs(self, node, adj, visited,stack):
+        visited[node] = True
+        for v in adj[node]:
+            if(not visited[v]):
+                self.dfs(v,adj,visited,stack)
+        stack.append(node)
+        
+    #Function to return list containing vertices in Topological order.
+    def topoSort(self, V, adj):
+        visited = [False for i in range(0,V)]
+        stack = []
+        for i in range(0,V):
+            if(not visited[i]):
+                self.dfs(i,adj, visited, stack)
+        stack.reverse()
+        return stack
+
+
+
+#{ 
+ # Driver Code Starts
+# Driver Program
+
+import sys
+sys.setrecursionlimit(10**6)
+        
+def check(graph, N, res):
+    if N!=len(res):
+        return False
+    map=[0]*N
+    for i in range(N):
+        map[res[i]]=i
+    for i in range(N):
+        for v in graph[i]:
+            if map[i] > map[v]:
+                return False
+    return True
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        e,N = list(map(int, input().strip().split()))
+        adj = [[] for i in range(N)]
+        
+        for i in range(e):
+            u,v=map(int,input().split())
+            adj[u].append(v)
+            
+        ob = Solution()
+        
+        res = ob.topoSort(N, adj)
+        
+        if check(adj, N, res):
+            print(1)
+        else:
+            print(0)
+# Contributed By: Harshit Sidhwa
+
+# } Driver Code Ends
