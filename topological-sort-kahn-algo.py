3fb68d47d66c942c50bbf090ec80f3c3cf921e9c
renukatamboli <tambolirenuka2@gmail.com>
2023-05-11 00:01:26 +0530

    Create topological-sort-kahn-algo.py

---
+++ topological-sort-kahn-algo.py
@@ -0,0 +1,67 @@
+class Solution:
+    
+    #Function to return list containing vertices in Topological order.
+    def topoSort(self, V, adj):
+        indegree = [0 for i in range(0,V)]
+        queue = []
+        topo = []
+        for i in range(0,V):
+            for v in adj[i]:
+                indegree[v]+=1
+        for i in range(0,V):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+        while len(queue)>0:
+            node = queue.pop(0)
+            topo.append(node)
+            for v in adj[node]:
+                indegree[v] -= 1
+                if indegree[v] == 0:
+                    queue.append(v)
+            
+        return topo    
+        # Code here
+
+
+
+#{ 
+ # Driver Code Starts
+# Driver Program
+
+import sys
+sys.setrecursionlimit(10**6)
+        
+def check(graph, N, res):
+    if N!=len(res):
+        return False
+    map=[0]*N
+    for i in range(N):
+        map[res[i]]=i
+    for i in range(N):
+        for v in graph[i]:
+            if map[i] > map[v]:
+                return False
+    return True
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        e,N = list(map(int, input().strip().split()))
+        adj = [[] for i in range(N)]
+        
+        for i in range(e):
+            u,v=map(int,input().split())
+            adj[u].append(v)
+            
+        ob = Solution()
+        
+        res = ob.topoSort(N, adj)
+        
+        if check(adj, N, res):
+            print(1)
+        else:
+            print(0)
+# Contributed By: Harshit Sidhwa
+
+# } Driver Code Ends
