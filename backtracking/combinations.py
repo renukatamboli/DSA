b5cdb21dccf44b026879042e008d13e106d398b0
renukatamboli <tambolirenuka2@gmail.com>
2024-11-28 00:32:05 +0530

    Update combinations.py

--- backtracking/combinations.py
+++ backtracking/combinations.py
@@ -1,18 +1,13 @@
 class Solution:
     def solve(self,n,k,ans,temp,i):
-        if i == n+1:
-            if len(temp) == k:
-                ans.append(temp[:])
-                temp = []
-            return
         if len(temp) == k:
             ans.append(temp[:])
-            temp = []
             return
+            
         for j in range(i,n+1):
             temp.append(j)
             self.solve(n,k,ans,temp,j+1)
-            temp.remove(j)
+            temp.pop()
             
     def combine(self, n: int, k: int) -> List[List[int]]:
         ans = []
