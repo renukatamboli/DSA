51dd16c26e8c84779768777e04d0950f53575869
renukatamboli <tambolirenuka2@gmail.com>
2024-12-23 11:57:30 +0530

    Update remove_duplicates_from_sorted_array.py

--- two_pointer/remove_duplicates_from_sorted_array.py
+++ two_pointer/remove_duplicates_from_sorted_array.py
@@ -2,14 +2,13 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         l = 0
         r = 1
-        t = 0
         n = len(nums)
-        while l < n and r < n and t <n:
-            if nums[t] != nums[r]:
-                nums[l+1] = nums[r]
+        while r < n:
+            if nums[l] != nums[r]:
                 l = l+1
-                t = r
+                nums[l] = nums[r]
             r = r+1
         return l+1
+
 
         
