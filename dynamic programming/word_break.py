73a2af571381e4af7da8e7aed5ae275d5e1672c2
renukatamboli <tambolirenuka2@gmail.com>
2025-01-06 23:57:48 +0530

    Create word_break.py

---
+++ dynamic programming/word_break.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        l = len(s)
+        dp = [False]*(l+1)
+        dp[l] = True
+        for i in range(l-1,-1,-1):
+            for w in wordDict:
+                if (i+len(w)) <= l and s[i:i+len(w)] == w:
+                    dp[i] = dp[i+len(w)]
+                if dp[i]:
+                    break
+        return dp[0]
+        
